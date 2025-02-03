ecd205f1ab36c737d38351d7ac19204d7cef52f5
Peter van Gennip <pvgennip@twentysevenagency.com>
2025-02-03 08:34:33 +0100

    feat: header

--- components/app/AppHeader.vue
+++ components/app/AppHeader.vue
@@ -1,8 +1,15 @@
 <template>
-  <header class="mb-8 bg-white py-6 shadow">
+  <header class="mb-12 bg-white py-6 shadow">
     <BaseGridContainer>
       <BaseGridCol100>
-        <h1 class="h1">Twentyseven Dashboard</h1>
+        <h1 class="h1 flex items-center">
+          <span class="mr-4 text-green-600">Twentyseven</span>
+          <span>Dashboard</span>
+          <Icon
+            name="ic:baseline-dashboard-customize"
+            class="ml-4 text-green-700"
+          />
+        </h1>
       </BaseGridCol100>
     </BaseGridContainer>
   </header>
